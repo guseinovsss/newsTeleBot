32ac4cff56c1885d402b89ec7564a584141923b9
cxllzx <61634078+guseinov275@users.noreply.github.com>
2022-02-05 21:00:52 +0300

    update and rewrite on aiogram

--- main.py
+++ main.py
@@ -1,141 +1,25 @@
-#импорт библиотек
-import telebot
-from bs4 import BeautifulSoup
-import requests
-from telepot.loop import MessageLoop
-import re
+import logging
+import aiogram
 
-# токен теле бота
-bot = telebot.TeleBot('1742310134:AAE_szafX_AUqtECbgVIr0u-OVyKbXSE8J4')
+# токен бота
+TOKEN = '1742310134:AAE_szafX_AUqtECbgVIr0u-OVyKbXSE8J4'
 
 
-
-# @bot.message_handler(commands = ['r'])
-# def news1(message):
-# 	vah = message
-#  	result = re.match(r'text*[^\n]', str(vah))
-# 	bot.send_message(message.chat.id, vah)
+# создание бота
+bot = aiogram.Bot(token="1742310134:AAE_szafX_AUqtECbgVIr0u-OVyKbXSE8J4")
 
 
-@bot.message_handler(commands = ['ria'])#декоратор с помощью которого наш бот будет реагировать на команду
-def news(message):
-	URL = 'https://ria.ru/world'
-	HEADERS = {
-		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'# юзер агент
-	}
-
-	response = requests.get(URL, headers = HEADERS)
-	soup = BeautifulSoup(response.content, 'html.parser')
-	texts = soup.findAll('a', 'list-item__title')
-
-	for i in range(len(texts[:-16]), -1, -1):
-		txt = str(i + 1) + ') ' + texts[i].text
-		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')#отправляем гиперсылку пользователю
+dp = aiogram.Dispatcher(bot)
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
 
 
-@bot.message_handler(commands = ['vottak'])#декоратор с помощью которого наш бот будет реагировать на команду
-def news(message):
-	URL = 'https://vot-tak.tv/novosti'
-	HEADERS = {
-		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
-	}
-	response = requests.get(URL, headers = HEADERS)
-	soup = BeautifulSoup(response.content, 'html.parser')
-	texts = soup.findAll('a', 'post_card_frame')
-	
-	for i in range(len(texts[:-12]), -1, -1):
-		txt = str(i + 1) + ') ' + texts[i].text
-		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')
+# Хэндлер на команду /test1
+@dp.message_handler(commands="test1")
+async def cmd_test1(message: aiogram.types.Message):
+    await message.reply("Test 1")
 
 
-@bot.message_handler(commands = ['rbcTrends'])#декоратор с помощью которого наш бот будет реагировать на команду
-def news(message):
-	URL = 'https://trends.rbc.ru/trends/?utm_source=topline'
-	HEADERS = {
-		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
-	}
-
-	response = requests.get(URL, headers = HEADERS)
-	soup = BeautifulSoup(response.content, 'html.parser')
-	texts = soup.findAll("a","item__title")
-
-	for i in range(len(texts[:4]), -1, -1):
-		txt = str(i + 1) + ') ' + texts[i].text
-		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')
-
-
-@bot.message_handler(commands = ['rbkNewspaper'])#декоратор с помощью которого наш бот будет реагировать на команду
-def news(message):
-	URL = 'https://www.rbc.ru/newspaper/?utm_source=topline'
-	HEADERS = {
-		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
-	}
-
-	response = requests.get(URL, headers = HEADERS)
-	soup = BeautifulSoup(response.content, 'html.parser')
-	texts = soup.findAll("a","newspaper-page__news")
-
-	for i in range(len(texts[:4]), -1, -1):
-		txt = str(i + 1) + ') ' + texts[i].text
-		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')
-
-@bot.message_handler(commands = ['rbk'])#декоратор с помощью которого наш бот будет реагировать на команду
-def news(message):
-	URL = 'https://www.rbc.ru/'
-	HEADERS = {
-		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
-	}
-
-	response = requests.get(URL, headers = HEADERS)
-	soup = BeautifulSoup(response.content, 'html.parser')
-	texts = soup.findAll("a","main__feed__link js-yandex-counter")
-
-	for i in range(len(texts[:4]), -1, -1):
-		txt = str(i + 1) + ') ' + texts[i].text
-		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')
-
-@bot.message_handler(commands = ['mailru'])#декоратор с помощью которого наш бот будет реагировать на команду
-def news(message):
-	URL = 'https://news.mail.ru/economics/'
-	HEADERS = {
-		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
-	}
-
-	response = requests.get(URL, headers = HEADERS)
-	soup = BeautifulSoup(response.content, 'html.parser')
-	texts = soup.findAll("a","newsitem__title link-holder")
-
-	for i in range(len(texts[:4]), -1, -1):
-		txt = str(i + 1) + ') ' + texts[i].text
-		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')
-
-
-@bot.message_handler(commands=['/findforhashtag'])
-def start_message(message):
-    bot.send_message(message.chat.id, '', reply_markup=keyboard)
-
-
-@bot.message_handler(commands=['start','back'])
-def start_message(message):
-    keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('/resours','/byVIP')
-    keyboard.row('/findforhashtag','/help')
-
-    bot.send_message(message.chat.id, 'выбери сайт', reply_markup=keyboard)
-
-@bot.message_handler(commands=['resours'])
-def start_message(message):
-    keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('/rbk', '/mailru','/vottak')
-    keyboard.row('/rbkNewspaper', '/ria','/rbcTrends')
-    keyboard.row('/back')
-    bot.send_message(message.chat.id, 'выбери сайт', reply_markup=keyboard)
-    
-@bot.message_handler(commands=['help'])
-def start_message(message):
-
-    bot.send_message(message.chat.id, '', reply_markup=keyboard)
-# SAID is programmer
-
-
-bot.polling(none_stop = True) 
+if __name__ == "__main__":
+    # Запуск бота
+    aiogram.executor.start_polling(dp, skip_updates=True)
---
+++ telebottest.py
@@ -0,0 +1,140 @@
+# #импорт библиотек
+# import telebot
+# from bs4 import BeautifulSoup
+# import requests
+
+# # токен теле бота
+# bot = telebot.TeleBot('1742310134:AAE_szafX_AUqtECbgVIr0u-OVyKbXSE8J4')
+
+# @bot.message_handler(commands = ['ria'])#декоратор с помощью которого наш бот будет реагировать на команду
+# def news(message):
+# 	URL = 'https://ria.ru/world'
+# 	HEADERS = {
+# 		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'# юзер агент
+# 	}
+
+# 	response = requests.get(URL, headers = HEADERS)
+# 	soup = BeautifulSoup(response.content, 'html.parser')
+# 	texts = soup.findAll('a', 'list-item__title')
+
+# 	for i in range(len(texts[:-16]), -1, -1):
+# 		txt = str(i + 1) + ') ' + texts[i].text
+# 		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')#отправляем гиперсылку пользователю
+
+
+# @bot.message_handler(commands = ['vottak'])#декоратор с помощью которого наш бот будет реагировать на команду
+# def news(message):
+# 	URL = 'https://vot-tak.tv/novosti'
+# 	HEADERS = {
+# 		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+# 	}
+# 	response = requests.get(URL, headers = HEADERS)
+# 	soup = BeautifulSoup(response.content, 'html.parser')
+# 	texts = soup.findAll('a', 'post_card_frame')
+	
+# 	for i in range(len(texts[:-12]), -1, -1):
+# 		txt = str(i + 1) + ') ' + texts[i].text
+# 		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')
+
+
+# @bot.message_handler(commands = ['rbcTrends'])#декоратор с помощью которого наш бот будет реагировать на команду
+# def news(message):
+# 	URL = 'https://trends.rbc.ru/trends/?utm_source=topline'
+# 	HEADERS = {
+# 		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+# 	}
+
+# 	response = requests.get(URL, headers = HEADERS)
+# 	soup = BeautifulSoup(response.content, 'html.parser')
+# 	texts = soup.findAll("a","item__title")
+
+# 	for i in range(len(texts[:4]), -1, -1):
+# 		txt = str(i + 1) + ') ' + texts[i].text
+# 		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')
+
+
+# @bot.message_handler(commands = ['rbkNewspaper'])#декоратор с помощью которого наш бот будет реагировать на команду
+# def news(message):
+# 	URL = 'https://www.rbc.ru/newspaper/?utm_source=topline'
+# 	HEADERS = {
+# 		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+# 	}
+
+# 	response = requests.get(URL, headers = HEADERS)
+# 	soup = BeautifulSoup(response.content, 'html.parser')
+# 	texts = soup.findAll("a","newspaper-page__news")
+
+# 	for i in range(len(texts[:4]), -1, -1):
+# 		txt = str(i + 1) + ') ' + texts[i].text
+# 		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')
+
+# @bot.message_handler(commands = ['rbk'])#декоратор с помощью которого наш бот будет реагировать на команду
+# def news(message):
+# 	URL = 'https://www.rbc.ru/'
+# 	HEADERS = {
+# 		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+# 	}
+
+# 	response = requests.get(URL, headers = HEADERS)
+# 	soup = BeautifulSoup(response.content, 'html.parser')
+# 	texts = soup.findAll("a","main__feed__link js-yandex-counter")
+
+# 	for i in range(len(texts[:4]), -1, -1):
+# 		txt = str(i + 1) + ') ' + texts[i].text
+# 		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')
+
+# @bot.message_handler(commands = ['mailru'])#декоратор с помощью которого наш бот будет реагировать на команду
+# def news(message):
+# 	URL = 'https://news.mail.ru/economics/'
+# 	HEADERS = {
+# 		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+# 	}
+
+# 	response = requests.get(URL, headers = HEADERS)
+# 	soup = BeautifulSoup(response.content, 'html.parser')
+# 	texts = soup.findAll("a","newsitem__title link-holder")
+
+# 	for i in range(len(texts[:4]), -1, -1):
+# 		txt = str(i + 1) + ') ' + texts[i].text
+# 		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')
+
+
+# # @bot.message_handler(commands=['/findforhashtag'])
+# # def start_message(message):
+# #     HEADERS = {
+# # 		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+# # 	}
+# #     question = 'https://www.google.com/search?q=новости '
+# # 	response = requests.get(question, headers = HEADERS)
+# # 	soup = BeautifulSoup(response.content, 'html.parser')
+# # 	text = soup.findAll("div","yuRUbf")
+
+# #     for i in range(len(texts[:4]), -1, -1):
+# # 		txt = str(i + 1) + ') ' + texts[i].text
+# # 		bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(text[i]['href'], txt), parse_mode = 'html')
+
+
+# @bot.message_handler(commands=['start','back'])
+# def start_message(message):
+#     keyboard = telebot.types.ReplyKeyboardMarkup(True)
+#     keyboard.row('/resours','/byVIP')
+#     keyboard.row('/findforhashtag','/help')
+
+#     bot.send_message(message.chat.id, 'выбери сайт', reply_markup=keyboard)
+
+# @bot.message_handler(commands=['resours'])
+# def start_message(message):
+#     keyboard = telebot.types.ReplyKeyboardMarkup(True)
+#     keyboard.row('/rbk', '/mailru','/vottak')
+#     keyboard.row('/rbkNewspaper', '/ria','/rbcTrends')
+#     keyboard.row('/back')
+#     bot.send_message(message.chat.id, 'выбери сайт', reply_markup=keyboard)
+    
+# @bot.message_handler(commands=['help'])
+# def start_message(message):
+
+#     bot.send_message(message.chat.id, '', reply_markup=keyboard)
+# # SAID is programmer
+
+
+# bot.infinity_poling(none_stop = True)
